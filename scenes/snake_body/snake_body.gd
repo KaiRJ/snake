1517457fb3c4a60ab398126f4c138d080c80a2ef
Kai Jenkins <kairjenkins@gmail.com>
2025-11-27 22:15:03 +0000

    End game when snake hits a wall

--- scenes/snake_body/snake_body.gd
+++ scenes/snake_body/snake_body.gd
@@ -3,9 +3,16 @@ class_name SnakeBody extends Node2D
 ##
 ## TODO detailed description
 
+# TODO might not need this when can just check the sprites directly
+var isHead := true
 
 func _ready() -> void:
 	$Area2D.area_entered.connect(_on_area_entered)
+
+
+## Turn body part into a snake body part
+func make_head():
+	pass
 
 
 ## Turn body part into a snake body part
@@ -19,6 +26,9 @@ func make_tail():
 
 
 func _on_area_entered(area: Area2D):
+	print("entered map bound")
 	# Only care if its the head of the snake
 	if area.get_parent() is SnakeManager:
+		GameEvents.game_over()
+	elif area.is_in_group("MapBounds"):
 		GameEvents.game_over()
--- scenes/snake_manager/snake_manager.gd
+++ scenes/snake_manager/snake_manager.gd
@@ -38,24 +38,31 @@ func _ready() -> void:
 	# Connect timer signal
 	$MovementTimer.timeout.connect(_on_movement_timer_timeout)
 
-	# Set the initial position to center (extra is cause sprites are centered)
 	if !tile_map_layer:
+		# TODO should error out
 		return
-		
+	
+	# Get tile width (assuming squares)
+	_tile_size = tile_map_layer.tile_set.tile_size
+	
+	# Get dimensions of tile map layer
 	var tm_rect := tile_map_layer.get_used_rect()
 	var tm_origin := tm_rect.position
 	var tm_size := tm_rect.size
-	_tile_size = tile_map_layer.tile_set._tile_size
 	
-	global_position = tm_origin + (_tile_size * (tm_size + Vector2i.ONE)/2)
-	
+	# Set the initial position to center (plus offset of sprite)
+	# TODO bound to be a better way of doing this
+	var center = _tile_size * Vector2i(tm_size.x/2, tm_size.y/2)
+	var offset = Vector2i(_tile_size/2)
+	global_position = tm_origin + center + offset
+
 	# Need an initial new direction
 	_new_direction = _current_direction
 
 	# Create the initial snake
-	for i in range(init_size-1):
-		add_snake_body_part()
-		move_snake_head()
+	for i in range(init_size):
+		print(i)
+		add_new_head_infront_of_head()
 
 
 func _physics_process(_delta: float) -> void:
@@ -67,7 +74,7 @@ func get_input() -> void:
 	for input in DIRECTIONS:
 		if Input.is_action_just_pressed(input):
 			_new_direction = DIRECTIONS[input]
-
+	
 
 ## Update _current_direction and _current_rotation based on _new_direction
 func update_rotation_and_direciton() -> void:
@@ -86,33 +93,40 @@ func calculate_rotation(new_dir: Vector2i) -> float:
 		return (new_dir.y * _current_direction.x) * PI/2
 
 
-## Add a new snake body part at current head position
-func add_snake_body_part() -> void:
-	var new_body := snake_body.instantiate() as SnakeBody
-	snake_body_parts.append(new_body)
-	new_body.rotation = rotation # Same rotation as the head
-	new_body.global_position = global_position # Same position as the head
-	$Body.add_child(new_body)
-
-
-## Move the snake head by one square in _current_direction
-func move_snake_head():
-	rotation = _current_rotation
-	global_position += Vector2(_current_direction * _tile_size)
+## Create a new head in front of the old head
+func add_new_head_infront_of_head() -> void:	
+	# instatiate a new snake body part and make it a head
+	var new_head := snake_body.instantiate() as SnakeBody
+	$Body.add_child(new_head)
+	move_part_to_front(new_head)
 
 
 ## Remove the current snake tail and make the new last body part a tail
-func remove_tail() -> void:
-	var current_tail = snake_body_parts.pop_front()
-	current_tail.queue_free()
-	
-	var new_tail = snake_body_parts.front()
-	new_tail.make_tail()
+func move_tail_infront_of_head() -> void:
+	# make current tail the new snake head
+	var new_head = snake_body_parts.pop_back()
+	new_head.make_head()
+	move_part_to_front(new_head)
+	snake_body_parts.back().make_tail() # replace the tail
+
+
+## Move an instance of a SnakeBody to the front of the snake
+func move_part_to_front(new_head: SnakeBody) -> void:
+	# If there is no head currently then use the position of the SnakeManager
+	var origin: Vector2i = global_position
+	if len(snake_body_parts) > 0:
+		var current_head = snake_body_parts.front() as SnakeBody
+		current_head.make_body()
+		origin = current_head.global_position 
+		
+	# initialise new head infront of the hold one
+	new_head.make_head()
+	new_head.rotation = _current_rotation # Same rotation as the head
+	new_head.global_position = origin + (_current_direction * _tile_size)
+	snake_body_parts.push_front(new_head)
 
 
 ## Update the players position on timer timeout
 func _on_movement_timer_timeout() -> void:
 	update_rotation_and_direciton()
-	add_snake_body_part()
-	move_snake_head()
-	remove_tail()
+	move_tail_infront_of_head()
